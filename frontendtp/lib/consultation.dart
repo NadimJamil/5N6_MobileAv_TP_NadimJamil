e7cc948f0acb94f9e5e8045a00fde787c8907ce3
Nadim Jamil <2261561@cegepmontpetit.ca>
2025-09-22 13:05:49 -0400

    FCT : Fin page consultation

--- frontendtp/lib/consultation.dart
+++ frontendtp/lib/consultation.dart
@@ -4,11 +4,14 @@ class consultation extends StatefulWidget {
   const consultation({super.key});
 
   final String title = "Consultation";
+
   @override
   State<consultation> createState() => _consultationState();
 }
 
 class _consultationState extends State<consultation> {
+  double rating = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +24,163 @@ class _consultationState extends State<consultation> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text("Tache #1")
+            const SizedBox(height: 100),
+            Container(
+              width: double.infinity,
+              height: 500,
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.black, width: 1),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Tâche #1",
+                    style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  Row(
+                    children: const [
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          "Nom :",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Text(
+                          "Bob",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+
+                  Row(
+                    children: const [
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          "Avancement :",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Text(
+                          "50%",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+
+                  Row(
+                    children: const [
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          "Deadline :",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Text(
+                          "2025-10-01",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Row(
+                    children: const [
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          "Pourcentage de temps écoulé :",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Text(
+                          "100%",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          "Slider : ",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Slider(
+                          value: rating,
+                          onChanged: (newRating) {
+                            setState(() => rating = newRating);
+                          },
+                          divisions: 10,
+                          label: rating.toStringAsFixed(1),
+                          min: 0,
+                          max: 5,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
